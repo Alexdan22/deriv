a5053fe02f769ef9c65f6cedc71856093c4ff8d0
Alex Daniel <cr3supprt@gmail.com>
2025-02-01 00:52:17 +0530

    response log added v3

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ const createWebSocketConnections = () => {
     ws.on('message', (data) => {
       const response = JSON.parse(data);
       if (response.msg_type === 'buy') {
-        console.log(`Buy response: ${response}`);
+        console.log(`Buy response: ${response.buy}`);
         
         const tradeKey = `frxXAUUSD-${response.buy.contract_id}`;
         if (!trades.has(tradeKey)) {
