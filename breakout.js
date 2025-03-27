12e879cc8b735a94da512fd5b7efcfa15d9b647b
Alex Daniel <cr3supprt@gmail.com>
2025-03-28 01:18:22 +0530

    Debugging and New bot added

--- breakout.js
+++ breakout.js
@@ -1009,8 +1009,8 @@ function startAtNextMinute() {
 
 
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
     startAtNextMinute();
   });
   
